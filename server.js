6d7143642d4fc07a6150aa397fe48e5bb81a576d
VanshK-MERN71 <vanshkhanna9140@gmail.com>
2026-02-27 13:05:04 +0530

    update server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://redux-frontend-brown.vercel.app",
+  "https://redux-frontend-pearl.vercel.app",
 ];
 
 app.use(
