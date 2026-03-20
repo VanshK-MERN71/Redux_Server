4f4c4820f35ba016dc9a7b1fdedc193765ac5d44
VanshK-MERN71 <vanshkhanna9140@gmail.com>
2026-03-20 11:58:50 +0530

    data

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,9 +7,9 @@ export const registerUser = async (req, res)=>{
     const name = normalizeString(req.body?.name);
     const email = normalizeString(req.body?.email).toLowerCase();
     const password = normalizeString(req.body?.password);
-
+    
+    console.log(name,email,password)
     if (!name || !email || !password) {
-        console.log(name,email,password)
         return res.status(400).json({ message: "Name, email and password are required" });
     }
     if (name.length < 3) {
